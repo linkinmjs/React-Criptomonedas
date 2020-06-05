a11d81e40a2e8ca016ab4930f886e1dc84013d69
Mauri <mauricio.stampella@25watts.com.ar>
2020-06-05 16:17:26 -0300

    Creando el Custom Hooks para las MonedaS

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import useMoneda from '../hooks/useMoneda';
 
 const Boton =  styled.input`
     margin-top: 20px;
@@ -20,9 +21,15 @@ const Boton =  styled.input`
 `   
 
 const Formulario = () => {
+
+    // Utilizar useMoneda
+    const[moneda,SelectMonedas, actualizarState] = useMoneda();
+
     return ( 
         <form>
             
+            <SelectMonedas />
+
             <Boton 
                 type="submit"
                 value="Calcular"
